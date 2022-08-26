f45a98d1d0ee4ba522d62874cc27e339ab3816e1
Mariane Candian <mariane.candian@gmail.com>
2022-08-26 16:59:47 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
 }
+
+console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
