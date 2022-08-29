b6a2167fc8d5dee9b75ba662215edb333c2555ca
Mariane Candian <mariane.candian@gmail.com>
2022-08-29 18:35:55 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      if (!acc[curr.name]) {
+        acc[curr.name] = curr.residents.length;
+      }
+      return acc;
+    }, {});
+  }
+  const especies = species.find((specie) => animal.specie === specie.name);
+  const speciesBySex = especies.residents.filter((resident) => resident.sex === animal.sex);
+  return (animal.sex) ? speciesBySex.length : especies.residents.length;
 }
+
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
