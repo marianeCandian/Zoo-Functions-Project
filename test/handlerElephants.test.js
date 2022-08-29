57f2ba88966d38a5e69cb92830c90605e98e995c
Mariane Candian <mariane.candian@gmail.com>
2022-08-29 15:43:22 -0300

    Requisito 6, 7 e 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,32 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verificando se a função retorna undefined quando não tem parâmetro', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('Verifica se ao entrar com um parâmetro que não seja string, retorne erro', () => {
+    expect(handlerElephants(6)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Verifica se ao receber count retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  it('Verifica se ao receber names retorna o nome Jefferson', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('verfica se ao receber averageAge retorna um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+  it('verfica se ao receber location retorna a localização do elefante dentro do zoo', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  it('verfica se ao receber populatiry retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('populatiry')).toBeNull();
+  });
+
+});
