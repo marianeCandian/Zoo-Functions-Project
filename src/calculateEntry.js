d52ccbb19856ee456a6883c44c8dff0a5d0e382c
Mariane Candian <mariane.candian@gmail.com>
2022-08-29 17:24:31 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
+function countEntrants(visitantes) {
+  const child = visitantes.filter((visitante) => visitante.age < 18).length;
+  const adult = visitantes.filter((visitante) => visitante.age >= 18 && visitante.age < 50).length;
+  const senior = visitantes.filter((visitante) => visitante.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants[0]) {
+    return 0;
+  }
+  {
+    const retorno = countEntrants(entrants);
+    return (retorno.child * 20.99 + retorno.adult * 49.99 + retorno.senior * 24.99);
+  }
 }
+
+// console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
