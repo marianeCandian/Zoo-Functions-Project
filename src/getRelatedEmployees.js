4a85cababded697bcfb5d7f12514dffbc4cb0f54
Mariane Candian <mariane.candian@gmail.com>
2022-08-29 17:59:55 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((funcionario) => funcionario.managers.includes(managerId))
+    .map((pessoas) => `${pessoas.firstName} ${pessoas.lastName}`);
 }
+
+// console.log(getRelatedEmployees('stephanieId'));
 
 module.exports = { isManager, getRelatedEmployees };
