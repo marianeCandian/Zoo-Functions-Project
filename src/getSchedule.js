784e0501c89784f6a0aada5e82017742fe907358
Mariane Candian <mariane.candian@gmail.com>
2023-01-24 14:53:11 -0300

    Commitando arquivos modificados

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
-function getSchedule() {
-  // seu código aqui
+const { species, hours } = data;
+
+const findAnimal = (animal) => species.find((specie) => specie.name === animal).availability;
+
+const arraySpecies = species.map((specie) => specie.name);
+
+const day = (schedule, scheduleTarget) => {
+  const dayObject = {};
+  dayObject[scheduleTarget] = schedule[scheduleTarget];
+  return dayObject;
+};
+
+const mondayClosed = () => {
+  const alerta = {
+    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+  };
+  return alerta;
+};
+
+const dayWeek = Object.keys(hours);
+
+const schedule = dayWeek.reduce((acc, curr) => {
+  acc[curr] = {
+    officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+    exhibition: species.filter((animal) => animal
+      .availability.includes(curr)).map((element) => element.name),
+  };
+  return acc;
+}, {});
+
+function getSchedule(scheduleTarget) {
+  if (scheduleTarget === 'Monday') return mondayClosed();
+
+  if (dayWeek.includes(scheduleTarget)) return day(schedule, scheduleTarget);
+
+  if (arraySpecies.includes(scheduleTarget)) return findAnimal(scheduleTarget);
+
+  return Object.assign(schedule, mondayClosed());
 }
 
 module.exports = getSchedule;
